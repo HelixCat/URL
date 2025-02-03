d667639672fa8116331c94b19f50fbe6bfb770c1
allahmoradi <allahmoradi@caspico.ir>
2025-02-03 13:52:24 +0330

    Initial commit

---
+++ src/main/java/com/mahdi/caspco/callbackurl/CallBackUrlApplication.java
@@ -0,0 +1,13 @@
+package com.mahdi.caspco.callbackurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallBackUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallBackUrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahdi/caspco/callbackurl/CallBackUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahdi.caspco.callbackurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallBackUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
